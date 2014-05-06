08ca15c563e11ed16950df55d721647e347f7e5e
rola <b.rolnik1@gmail.com>
2014-05-06 20:08:24 +0200

    utworzenie projektu - ldz_polesie_app

---
+++ ldz_polesie_app/entities/src/main/java/com/ldz/polesie/entities/Role.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ldz.polesie.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+/**
+ *
+ * @author Rola Entity class which contains user roles
+ *
+ */
+@Entity
+@Table(
+        name = "ROLE_T",
+        uniqueConstraints
+        = {
+              @UniqueConstraint(columnNames = "ROLE_NAME")
+          }
+)
+public class Role implements Serializable {
+   
+    private Long id;
+    private String roleName;
+    private List<User> users;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ROLE_ID", unique = true, nullable = false)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "ROLE_NAME", unique = true, nullable = false)
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @ManyToMany(mappedBy = "roles")
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + Objects.hashCode(this.id);
+        hash = 67 * hash + Objects.hashCode(this.roleName);
+        hash = 67 * hash + Objects.hashCode(this.users);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Role other = (Role) obj;
+        return Objects.equals(this.roleName, other.roleName);
+    }   
+}
---
+++ ldz_polesie_app/entities/src/main/java/com/ldz/polesie/entities/User.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ldz.polesie.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+/**
+ *
+ * @author Rola
+ *
+ * Entity class which contains users data needed to logging to system
+ *
+ */
+@Entity
+@Table(
+        name = "USER_T",
+        uniqueConstraints
+        = {
+            @UniqueConstraint(columnNames = "LOGIN")
+        }
+)
+public class User implements Serializable {
+
+    private Long id;
+    private String login;
+    private String password;
+    private Set<Role> roles;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "USER_ID", unique = true, nullable = false)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "LOGIN", unique = true, nullable = false, length = 255)
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    @Column(name = "PASSWORD", unique = true, nullable = false, length = 255)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "USER_ROLES_T",
+            joinColumns
+            = {
+                @JoinColumn(name = "USER_ID", nullable = false)
+              },
+            inverseJoinColumns
+            = {
+                @JoinColumn(name = "ROLE_ID", nullable = false)
+              }
+    )
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + Objects.hashCode(this.id);
+        hash = 53 * hash + Objects.hashCode(this.login);
+        hash = 53 * hash + Objects.hashCode(this.password);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        return Objects.equals(this.login, other.login);
+    }
+}
